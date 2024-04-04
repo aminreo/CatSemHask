57bce0e7d9600402351afd26108e9c4b1f5148aa
Amin <ayari.am@campus.lmu.de>
2024-04-04 11:35:56 +0200

    added docs

--- src/Hask/Category.hs
+++ src/Hask/Category.hs
@@ -74,31 +74,43 @@ type family Op (p :: i -> i -> *) :: i -> i -> * where
 -- C :: C^op -> [ C, Set ]
 -- @
 
+-- | The 'Category'' class represents a category in category theory.
+-- It defines the basic operations and constraints for a category.
 class Category' (p :: i -> i -> *) where
+  -- | The 'Ob' type family specifies the constraint on objects in the category.
   type Ob p :: i -> Constraint
+  -- | The 'id' function returns the identity morphism for a given object.
   id :: Ob p a => p a a
-  observe :: p a b -> Dict (Ob p a, Ob p b)
+  -- | The 'observe' function takes a morphism and returns a dictionary of constraints
+  -- on the source and target objects of the morphism.
+  observe :: p a b -> Dict (Ob p a, Ob p b) 
+  -- | The composition operator ('.') combines two morphisms to produce a new morphism.
   (.) :: p b c -> p a b -> p a c
 
+  -- | The 'unop' function converts an opposite morphism to its corresponding morphism.
   unop :: Op p b a -> p a b
   default unop :: Op p ~ Yoneda p => Op p b a -> p a b
   unop = getOp
 
+  -- | The 'op' function converts a morphism to its corresponding opposite morphism.
   op :: p b a -> Op p a b
   default op :: Op p ~ Yoneda p => p b a -> Op p a b
   op = Op
 
+-- | The 'Endo' type synonym represents an endomorphism, which is a morphism from an object to itself.
 type Endo p a = p a a
 
 --------------------------------------------------------------------------------
 -- * Functors
 --------------------------------------------------------------------------------
 
+-- | The 'Functor' class represents functors between categories.
 class (Category' (Dom f), Category' (Cod f)) => Functor (f :: i -> j) where
   type Dom f :: i -> i -> *
   type Cod f :: j -> j -> *
   fmap :: Dom f a b -> Cod f (f a) (f b)
 
+-- | The 'FunctorOf' class represents functors between categories with specific domain and codomain categories.
 class (Functor f, Dom f ~ p, Cod f ~ q) => FunctorOf p q f
 instance (Functor f, Dom f ~ p, Cod f ~ q) => FunctorOf p q f
 
@@ -107,6 +119,7 @@ ob :: forall f a. Functor f => Ob (Dom f) a :- Ob (Cod f) (f a)
 ob = Sub $ case observe (fmap (id :: Dom f a a) :: Cod f (f a) (f a)) of
   Dict -> Dict
 
+-- | The 'Nat' data type represents natural transformations between functors.
 data Nat (p :: i -> i -> *) (q :: j -> j -> *) (f :: i -> j) (g :: i -> j) where
   Nat :: ( FunctorOf p q f
          , FunctorOf p q g
@@ -117,6 +130,7 @@ data Nat (p :: i -> i -> *) (q :: j -> j -> *) (f :: i -> j) (g :: i -> j) where
 -- | Endomorphisms of a given functor
 type NatId p = Endo (Nat (Dom p) (Cod p)) p
 
+-- | The 'obOf' function checks if the given natural transformation and object satisfy the required constraints.
 obOf :: (Category (Dom f), Category (Cod f)) => NatId f -> Endo (Dom f) a
      -> Dict (Ob (Nat (Dom f) (Cod f)) f, Ob (Dom f) a, Ob (Cod f) (f a))
 obOf f a = case observe f of
@@ -124,42 +138,60 @@ obOf f a = case observe f of
     Dict -> case observe (f ! a) of
       Dict -> Dict
 
+-- | The 'Copresheaves' type represents copresheaves in a category.
 type Copresheaves p = Nat p (->)
+
+-- | The 'Presheaves' type represents presheaves in a category.
 type Presheaves p = Nat (Op p) (->)
 
+-- | This instance defines the 'Functor' instance for the 'FunctorOf' type, which is a type-level representation of a functor between two categories.
+-- It requires that both the source and target categories are instances of the 'Category'' type class.
 instance (Category' p, Category' q) => Functor (FunctorOf p q) where
+  -- | Type synonym for the domain of a functor.
   type Dom (FunctorOf p q) = Nat p q
+
+  -- | Type synonym for the codomain of a functor.
   type Cod (FunctorOf p q) = (:-)
+
+  -- | Functor instance for the 'Nat' type.
   fmap Nat{} = Sub Dict
 
 --------------------------------------------------------------------------------
 -- * Bifunctors
 --------------------------------------------------------------------------------
-
+-- | Type family to extract the domain of a bifunctor.
 type family NatDom (f :: (i -> j) -> (i -> j) -> *) :: (i -> i -> *) where
   NatDom (Nat p q) = p
 
+-- | Type family to extract the codomain of a bifunctor.
 type family NatCod (f :: (i -> j) -> (i -> j) -> *) :: (j -> j -> *) where
   NatCod (Nat p q) = q
 
+-- | Type synonym to simplify accessing the domain of a bifunctor.
 type Dom2 p = NatDom (Cod p)
+
+-- | Type synonym to simplify accessing the codomain of a bifunctor.
 type Cod2 p = NatCod (Cod p)
 
+-- | Class representing a bifunctor.
 class (Functor p, Cod p ~ Nat (Dom2 p) (Cod2 p), Category' (Dom2 p), Category' (Cod2 p)) => Bifunctor (p :: i -> j -> k)
 instance  (Functor p, Cod p ~ Nat (Dom2 p) (Cod2 p), Category' (Dom2 p), Category' (Cod2 p)) => Bifunctor (p :: i -> j -> k)
 
+-- | Functor map operation for a bifunctor.
 fmap1 :: forall p a b c. (Bifunctor p, Ob (Dom p) c) => Dom2 p a b -> Cod2 p (p c a) (p c b)
 fmap1 f = case ob :: Ob (Dom p) c :- FunctorOf (Dom2 p) (Cod2 p) (p c) of
   Sub Dict -> fmap f
 
-
+-- | Bifunctor map operation for two domains.
 bimap :: Bifunctor p => Dom p a b -> Dom2 p c d -> Cod2 p (p a c) (p b d)
 bimap f g = case observe f of
   Dict -> case observe g of
     Dict -> runNat (fmap f) . fmap1 g
 
+-- | Type synonym to simplify accessing the opposite category of a functor.
 type Opd f = Op (Dom f)
 
+-- | Contravariant map operation for a functor.
 contramap :: Functor f => Opd f b a -> Cod f (f a) (f b)
 contramap = fmap . unop
 
@@ -170,6 +202,12 @@ contramap = fmap . unop
 -- The variance here matches Haskell's order, which means that the contravariant
 -- argument comes first!
 
+-- | Maps over both the input and output of a bifunctor using two separate functions.
+--
+-- This function takes an `Opd` (opposite category) morphism `Opd p b a` and a `Dom2` (domain) morphism `Dom2 p c d`,
+-- and returns a `Cod2` (codomain) morphism `Cod2 p (p a c) (p b d)`.
+--
+-- The `dimap` function is implemented by composing the `bimap` function with the `unop` function.
 dimap :: Bifunctor p => Opd p b a -> Dom2 p c d -> Cod2 p (p a c) (p b d)
 dimap = bimap . unop
 
@@ -195,14 +233,17 @@ instance (Category'' p, Category'' (Op p), p ~ Op (Op p), Ob p ~ Ob (Op p)) => C
 -- * Vacuous
 --------------------------------------------------------------------------------
 
+-- | The 'Vacuous' class represents a vacuous category where all objects are the same.
 class Vacuous (c :: i -> i -> *) (a :: i)
 instance Vacuous c a
 
+-- | The 'Functor' instance for 'Dict' represents a functor that maps between constraints.
 instance Functor Dict where
   type Dom Dict = (:-)
   type Cod Dict = (->)
   fmap f Dict = case f of Sub g -> g
 
+-- | The 'Functor' instance for 'Vacuous c' represents a functor that maps between objects in the 'c' category.
 instance (Category' c, Ob c ~ Vacuous c) => Functor (Vacuous c) where
   type Dom (Vacuous c) = c
   type Cod (Vacuous c) = (:-)
@@ -212,16 +253,19 @@ instance (Category' c, Ob c ~ Vacuous c) => Functor (Vacuous c) where
 -- * The Category of Constraints
 --------------------------------------------------------------------------------
 
+-- | The 'Functor' instance for @(->)@.
 instance Functor (:-) where
   type Dom (:-) = Op (:-)
   type Cod (:-) = Nat (:-) (->) -- copresheaves
   fmap (Op f) = Nat (. f)
 
+-- | The 'Functor' instance for @(->) b@.
 instance Functor ((:-) b) where
   type Dom ((:-) b) = (:-)
   type Cod ((:-) b) = (->)
   fmap = (.)
 
+-- | The 'Category' instance for @(->)@.
 instance Category' (:-) where
   type Ob (:-) = Vacuous (:-)
   id = Constraint.refl
@@ -229,6 +273,7 @@ instance Category' (:-) where
   (.) = Constraint.trans
   unop = getOp
 
+-- | Convert a function that requires a constraint into a morphism in the category @(->)@.
 sub :: (a => Proxy a -> Dict b) -> a :- b
 sub k = Sub (k Proxy)
 
@@ -236,16 +281,19 @@ sub k = Sub (k Proxy)
 -- * Hask
 --------------------------------------------------------------------------------
 
+-- | 'Functor' instance for the function arrow (->).
 instance Functor (->) where
   type Dom (->) = Op (->)
   type Cod (->) = Nat (->) (->)
   fmap (Op f) = Nat (. f)
 
+-- | 'Functor' instance for functions with a fixed argument 'a'.
 instance Functor ((->)a) where
   type Dom ((->) a) = (->)
   type Cod ((->) a) = (->)
   fmap = (.)
 
+-- | 'Category'' instance for the function arrow (->).
 instance Category' (->) where
   type Ob (->) = Vacuous (->)
   id x = x
@@ -256,17 +304,33 @@ instance Category' (->) where
 --------------------------------------------------------------------------------
 -- * Yoneda :: i -> [ Op i, Set ]
 --------------------------------------------------------------------------------
-
+-- | This module defines instances for the 'Functor' and 'Category'' typeclasses for the 'Yoneda' type.
+-- The 'Yoneda' type represents the Yoneda embedding of a category.
+-- It provides a way to lift a category into a functor category.
+-- The instances defined here require that the underlying category 'p' is an instance of 'Category' and that its opposite category 'Op p' is isomorphic to 'Yoneda p'.
+-- The 'Functor' instance for 'Yoneda p' lifts a morphism in the category 'p' to a natural transformation in the functor category 'Nat (Yoneda p) (->)'.
+-- The 'Functor' instance for 'Yoneda p a' lifts a morphism in the Yoneda embedding of 'p' to a morphism in the category '(->)'.
+-- The 'Category'' instance for 'Yoneda p' defines the identity morphism, composition of morphisms, and the 'observe' function for the Yoneda embedding of 'p'.
+-- The 'Ob' associated type specifies the objects of the Yoneda embedding, which are the same as the objects of the underlying category 'p'.
+-- The 'id' function lifts the identity morphism of 'p' to the Yoneda embedding.
+-- The 'Op' function lifts a morphism in the Yoneda embedding to a morphism in the underlying category 'p'.
+-- The 'observe' function checks if a morphism in the Yoneda embedding is an isomorphism by observing its behavior on objects.
+-- The 'unop' function lifts a morphism in the Yoneda embedding to a morphism in the opposite category 'Op p'.
+-- The 'op' function retrieves the underlying morphism from the lifted morphism in the Yoneda embedding.
 instance (Category p, Op p ~ Yoneda p) => Functor (Yoneda p) where
   type Dom (Yoneda p) = p
   type Cod (Yoneda p) = Nat (Yoneda p) (->)
   fmap f = Nat (. Op f)
 
+-- | This instance defines the 'Functor' typeclass for the 'Yoneda' type with a specific object 'a'.
+-- It lifts a morphism in the Yoneda embedding of 'p' to a morphism in the category '(->)'.
 instance (Category p, Op p ~ Yoneda p) => Functor (Yoneda p a) where
   type Dom (Yoneda p a) = Yoneda p
   type Cod (Yoneda p a) = (->)
   fmap = (.)
 
+-- | This instance defines the 'Category'' typeclass for the 'Yoneda' type.
+-- It provides the identity morphism, composition of morphisms, and the 'observe' function for the Yoneda embedding of 'p'.
 instance (Category p, Op p ~ Yoneda p) => Category' (Yoneda p) where
   type Ob (Yoneda p) = Ob p
   id = Op id
@@ -280,28 +344,34 @@ instance (Category p, Op p ~ Yoneda p) => Category' (Yoneda p) where
 -- * Nat
 --------------------------------------------------------------------------------
 
+-- | The 'Functor' instance for natural transformations between categories.
 instance (Category' p, Category q) => Functor (Nat p q) where
   type Dom (Nat p q) = Op (Nat p q)
   type Cod (Nat p q) = Nat (Nat p q) (->)
   fmap (Op f) = Nat (. f)
 
+-- | The 'Functor' instance for natural transformations between categories and a specific type 'a'.
 instance (Category' p, Category q) => Functor (Nat p q a) where
   type Dom (Nat p q a) = Nat p q
   type Cod (Nat p q a) = (->)
   fmap = (.)
 
+-- | The 'Category'' instance for natural transformations between categories.
 instance (Category' p, Category' q) => Category' (Nat p q) where
    type Ob (Nat p q) = FunctorOf p q
    id = Nat id1 where
+     -- | The identity natural transformation.
      id1 :: forall f x. (Functor f, Dom f ~ p, Cod f ~ q, Ob p x) => q (f x) (f x)
      id1 = id \\ (ob :: Ob p x :- Ob q (f x))
    observe Nat{} = Dict
    Nat f . Nat g = Nat (f . g)
    unop = getOp
 
+-- | Create a natural transformation between categories 'p' and 'q' from a polymorphic function.
 nat :: (Category p ,Category q, FunctorOf p q f, FunctorOf p q g) => (forall a. Ob p a => Endo p a -> q (f a) (g a)) -> Nat p q f g
 nat k = Nat (k id)
 
+-- | Apply a natural transformation to a morphism in category 'p'.
 infixr 1 !
 (!) :: Nat p q f g -> p a a -> q (f a) (g a)
 Nat n ! f = case observe f of
@@ -311,33 +381,40 @@ Nat n ! f = case observe f of
 -- * Instances
 --------------------------------------------------------------------------------
 
+-- | 'Functor' instance for the tuple type '(,)'. It maps a function over the first element of the tuple.
 instance Functor (,) where
   type Dom (,) = (->)
   type Cod (,) = Nat (->) (->)
   fmap f = Nat $ \(a,b) -> (f a, b)
 
+-- | 'Functor' instance for the partially applied tuple type '(,) a'. It maps a function over the second element of the tuple.
 instance Functor ((,) a) where
   type Dom ((,) a) = (->)
   type Cod ((,) a) = (->)
   fmap f (a,b) = (a, f b)
 
+-- | 'Functor' instance for the 'Either' type. It maps a function over the left value of the 'Either' type.
 instance Functor Either where
   type Dom Either = (->)
   type Cod Either = Nat (->) (->)
   fmap f0 = Nat (go f0) where
+    -- | Helper function that applies the function 'f' to the left value of the 'Either' type.
     go :: (a -> b) -> Either a c -> Either b c
     go f (Left a)  = Left (f a)
     go _ (Right b) = Right b
 
+-- | 'Functor' instance for 'Either' type.
 instance Functor (Either a) where
   type Dom (Either a) = (->)
   type Cod (Either a) = (->)
   fmap _ (Left a) = Left a
   fmap f (Right b) = Right (f b)
 
+-- | Applies a natural transformation to the first component of a functor.
 first :: (Functor f, Cod f ~ Nat d e, Ob d c) => Dom f a b -> e (f a c) (f b c)
 first = runNat . fmap
 
+-- | 'Functor' instance for 'Fix' type.
 newtype Fix (f :: * -> * -> *) (a :: *) = In { out :: f (Fix f a) a }
 
 instance Functor Fix where
@@ -346,6 +423,7 @@ instance Functor Fix where
   fmap f = case observe f of 
     Dict -> Nat $ \ (In mu) -> In (first (first f) (runNat (runNat f) mu))
 
+-- | 'Functor' instance for 'Fix' type with constraints on the category.
 instance FunctorOf (->) (Nat (->) (->)) p => Functor (Fix p) where
   type Dom (Fix p) = (->)
   type Cod (Fix p) = (->)
@@ -388,7 +466,7 @@ data Rel a b = Rel { rel :: Set (a, b) }
 instance Category' Rel where
   type Ob Rel = Vacuous Rel
   id = Rel Set.empty
-  (Rel r) . (Rel s) = undefined-- Rel (Set.fromList [ (x, z) | (x, y) <- Set.toList (rel s), (y', z) <- Set.toList (rel r), y == y' ])
+  (Rel r) . (Rel s) = Rel (Set.fromList [ (x, z) | (x, y) <- Set.toList (rel s), (y', z) <- Set.toList (rel r), y == y' ])
   observe _ = Dict
 
 -- Initial object instance for Rel
@@ -398,21 +476,19 @@ instance InitialObject Rel () where
 instance TerminalObject Rel () where
   terminalArrow = Rel Set.empty
 
+-- Test initial object instance
+testInitial :: Bool
+testInitial = let rel' = initialArrow :: Rel () () in Set.null (rel rel')
+
+-- Test terminal object instance
+testTerminal :: Bool
+testTerminal = let rel' = terminalArrow :: Rel () () in Set.null (rel rel')
+
 testRelCategory :: IO ()
 testRelCategory = do
   putStrLn "Testing Rel Category (Sets and Relations):"
-  
-  -- Test initial object (empty relation)
-  let initialRel = rel (initialArrow :: Rel () Int)
-  putStrLn $ "Initial Relation: " ++ show initialRel
-  
-  -- Test terminal object (empty relation)
-  let terminalRel = rel (terminalArrow :: Rel Int ())
-  putStrLn $ "Terminal Relation: " ++ show terminalRel
-  
-  -- Check if initial and terminal objects are indeed empty relations
-  putStrLn $ "Is Initial Relation empty? " ++ show (Set.null initialRel)
-  putStrLn $ "Is Terminal Relation empty? " ++ show (Set.null terminalRel)
+  putStrLn $ "Initial object test: " ++ show testInitial
+  putStrLn $ "Terminal object test: " ++ show testTerminal
 
 --------------------------------------------------------------------------------
 -- Any partially ordered set (P, ≤) can be interpreted as a category: 
@@ -422,11 +498,13 @@ testRelCategory = do
 --------------------------------------------------------------------------------
 data Poset a b = Poset { poset :: Set (a, b) }
 
+-- Poset s1 _ = Set (s1,_) 
+-- (b,_)(a,_)
 instance Category' Poset where
   type Ob Poset = Ord
   id = Poset Set.empty
-  Poset s1 . Poset s2 = undefined -- Poset (Set.fromList [ (x, y) | x <- Set.toList s1, y <- Set.toList s2, x <= y ])
-  observe _ = Dict
+  Poset s1 . Poset s2 =  Poset (Set.fromList [ (x, y) | x <- Set.toList s1, y <- Set.toList s2, x <= y ])
+  observe (Poset a) = Dict
 
 -- Define the initial object for Poset
 instance InitialObject Poset Int where
@@ -456,31 +534,38 @@ testPosetCategory = do
 -- In Ring, the category of rings with unity and unity-preserving morphisms,
 -- the ring of integers Z is an initial object. The zero ring consisting only 
 -- of a single element 0 = 1 is a terminal object.
---------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------
+-- Define a typeclass for rings
 class Ring a where
   unity :: a
   add :: a -> a -> a
   mul :: a -> a -> a
   neg :: a -> a
 
+-- Define the data type for the ring of integers (initial object)
 data Z = Z
 
+-- Define Z as an instance of the Ring typeclass
 instance Ring Z where
   unity = Z
   add _ _ = Z
   mul _ _ = Z
   neg _ = Z
 
+-- Define the data type for the zero ring (terminal object)
 data ZeroRing = ZeroRing
 
+-- Define ZeroRing as an instance of the Ring typeclass
 instance Ring ZeroRing where
   unity = ZeroRing
   add _ _ = ZeroRing
   mul _ _ = ZeroRing
   neg _ = ZeroRing
 
+-- Define a data type for representing morphisms in the category of rings
 data RingMorphism a b = RingMorphism { ringMorphism :: a -> b }
 
+-- Define RingMorphism as an instance of Category' typeclass
 instance Category' RingMorphism where
   type Ob RingMorphism = Ring
   id = RingMorphism Prelude.id
